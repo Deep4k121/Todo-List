c4b57cc6c19019b9f71465a1894c37f2967c982d
Deepak <97617653+Deep4k121@users.noreply.github.com>
2022-07-07 12:04:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import Header from './Components/Header';
+import { Todos } from "./Components/Todos";
+import { Footer } from "./Components/Footer";
+import { AddTodo } from "./Components/AddTodo";
+import React, { useState, useEffect } from 'react';
+function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+  const addTodo = (title, desc) => {
+
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      description: desc
+    }
+    setTodos([...todos, myTodo]);
+  }
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+  return (
+    <>
+      <Header title="My-TodosList" searchBar={false} />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+export const Footer = () => {
+ 
+  return (
+    <div className='bg-dark text-light py-2'>
+      <p className='text-center'>
+
+        Copyright &copy; Deep4kTodosList.com
+      </p>
+    </div>
+  )
+}
